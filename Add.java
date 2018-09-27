b088bfaaa6fa8ae94cc7b812dff156d0a7a9f94b
RaquelSerena <43632008+RaquelSerena@users.noreply.github.com>
2018-09-26 23:03:04 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,37 @@
+
+public class Add {
+    
+    public Add() {
+        
+    }  
+    private Node predecessor; // Node predecessor
+    private Node destination;// Node destination
+    private int duration;// Node duration
+
+    //getters setters
+
+    public Node getPredecessor() {
+        return predecessor;
+    }
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setPredecessor(Node predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public void setDestination(Node destination) {
+        this.destination = destination;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+}
---
+++ Node.java
@@ -0,0 +1,43 @@
+
+import java.awt.Point;
+
+
+public class Node {
+    
+    public Node() {
+        point = new Point();
+    }
+    private String activityName; // name of the activity
+    private Point point; // location of the node 
+    private int activityDuration; 
+
+    //getters setters
+
+ 
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+    
+    // .
+    public void setActivityDuration(int activityDuration) {
+        this.activityDuration = activityDuration;
+    }
+
+    public int getActivityDuration() {
+        return activityDuration;
+    }
+    
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+    
+}
