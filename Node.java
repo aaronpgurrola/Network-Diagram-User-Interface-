cb8a80a15ae539475da3cb639a114bfe767912c0
aaronpgurrola <43392595+aaronpgurrola@users.noreply.github.com>
2018-09-27 19:41:01 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -7,13 +7,55 @@ public class Node {
     public Node() {
         point = new Point();
     }
-    private String activityName; // name of the activity
-    private Point point; // location of the node 
+    
+    /*NODE VARIABLES*/
+    
+    private String activityName; 
+    // name of the activity
+    
+    private Point point; 
+    // location of the node 
+    
     private int activityDuration; 
-
-    //getters setters
-
- 
+    //duration of the activity
+    
+    private int numPredecessors = 0;
+    //number of predecessors
+    
+    private boolean isHead; 
+    //boolean to see if node is head  
+    //only one node may be head
+    
+    private boolean isTail = true; 
+    //boolean to see if node is tail
+    //processing of paths must be used with tails
+    //if node has no successors, it must be a tail
+    //node is indicated as true until made into a predecessor
+    
+    /*END NODE VARIABLES*/
+    
+    
+    /*GETTERS & SETTERS*/
+    
+    //HEADS & TAILS
+    public void setHead (boolean isHead){
+        this.isHead = isHead;
+    }
+    
+    public boolean isHead() {
+        return isHead;
+    }
+    
+    public void setTail (boolean isTail) {
+        this.isTail = isTail;
+    }
+    
+    public boolean isTail() {
+        return isTail;
+    }
+    //END HEADS & TAILS
+    
+    
     public void setActivityName(String activityName) {
         this.activityName = activityName;
     }
@@ -22,7 +64,6 @@ public class Node {
         return activityName;
     }
     
-    // .
     public void setActivityDuration(int activityDuration) {
         this.activityDuration = activityDuration;
     }
@@ -38,6 +79,17 @@ public class Node {
 
     public void setPoint(Point point) {
         this.point = point;
+    }
+    
+    public int getNumPredecessors() {
+        return numPredecessors;
+    }
+    /*END GETTERS & SETTERS*/
+    
+    /*OTHER METHODS*/
+    
+    public void iteratePredecessors() {
+        numPredecessors++;
     }
     
 }
