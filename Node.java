51947e4d1b91aabacecb5cdddb79691446fa1515
aaronpgurrola <43392595+aaronpgurrola@users.noreply.github.com>
2018-10-11 14:00:55 -0700

    Update Node.java
    made many additions to node.java for processing

--- Node.java
+++ Node.java
@@ -1,6 +1,7 @@
 package netgraph;
 import java.awt.Point;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Node {
     
     public Node() {
@@ -9,8 +10,14 @@ public class Node {
     private String activityName; // name of the activity
     private Point point; // location of the node 
     private int activityDuration; 
-
-    // Getter and Setter methods 
+    public List <Node> children = new ArrayList<>();
+    private boolean isHead = false;
+    private short processed = 0;
+    private boolean hasPredecessor = false;
+    
+    
+    
+    //getters setters
     public void setActivityName(String activityName) {
         this.activityName = activityName;
     }
@@ -19,6 +26,7 @@ public class Node {
         return activityName;
     }
     
+    // .
     public void setActivityDuration(int activityDuration) {
         this.activityDuration = activityDuration;
     }
@@ -27,12 +35,45 @@ public class Node {
         return activityDuration;
     }
     
+
     public Point getPoint() {
         return point;
     }
 
     public void setPoint(Point point) {
         this.point = point;
+    }
+    
+    public void setHead(boolean isHead) {
+    	this.isHead = isHead; 
+    }
+    
+    public boolean isTail() {
+    	return (children.isEmpty());
+    }
+    
+    public void add(Node n) {
+    	children.add(n);
+    }
+    
+    public void reset() {
+    	processed = 0;
+    }
+    
+    public void process() {
+    	processed++;
+    }
+    
+    public int getProcessed() {
+    	return processed;
+    }
+    
+    public void doesHasPredecessor() {
+    	hasPredecessor = true;
+    }
+    
+    public boolean hasPredecessor() {
+    	return hasPredecessor;
     }
     
 }
